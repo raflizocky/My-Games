b8ae8a1f280009212cecc0555bc0d31fdebedf9c
rafli zocky <raflizocky@gmail.com>
2024-09-02 21:16:29 +0700

    add recycler view, action bar, about page

--- app/src/main/java/com/example/mygames/MainActivity.kt
+++ app/src/main/java/com/example/mygames/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.mygames
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rvGames: RecyclerView
+    private val list = ArrayList<Game>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,44 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        rvGames = findViewById(R.id.rv_games)
+        rvGames.setHasFixedSize(true)
+        list.addAll(getListGames())
+        showRecyclerList()
+    }
+
+    private fun getListGames(): ArrayList<Game> {
+        val dataName = resources.getStringArray(R.array.data_name)
+        val dataDescription = resources.getStringArray(R.array.data_description)
+        val dataPhoto = resources.obtainTypedArray(R.array.data_photo)
+
+        val listGame = ArrayList<Game>()
+        for (i in dataName.indices) {
+            val game = Game(dataName[i], dataDescription[i], dataPhoto.getResourceId(i, -1))
+            listGame.add(game)
+        }
+        return listGame
+    }
+
+    private fun showRecyclerList() {
+        rvGames.layoutManager = LinearLayoutManager(this)
+        val listGameAdapter = ListGameAdapter(list)
+        rvGames.adapter = listGameAdapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_about -> {
+                val intent = Intent(this, AboutActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
