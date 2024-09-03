33ef1562c17c1a291473034d5fe3038ccaa27145
rafli zocky <raflizocky@gmail.com>
2024-09-03 08:33:20 +0700

    add about page (library, intent, style)

--- app/src/main/java/com/example/mygames/AboutActivity.kt
+++ app/src/main/java/com/example/mygames/AboutActivity.kt
@@ -1,6 +1,7 @@
 package com.example.mygames
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,16 @@ class AboutActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
